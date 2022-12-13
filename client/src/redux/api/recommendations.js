f29b2a22c2476f3dff4b7709987dd8ad7df4e1a1
Sehajpreet Kaur <84325400+sehajbajaj@users.noreply.github.com>
2022-12-13 20:50:54 +0530

    Update recommendations.js

--- client/src/redux/api/recommendations.js
+++ client/src/redux/api/recommendations.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 /* For deployment purposes add the link to your website instead */
 // const url = "http://127.0.0.1:8000/api/v1/anime";
-const url = "https://animeculture.up.railway.app/api/v1/anime";
+const url = "https://animesculture.up.railway.app/api/v1/anime";
 export const recommendAnime = (animeId) =>
   axios.get(`${url}/${animeId}/recommend/`);
